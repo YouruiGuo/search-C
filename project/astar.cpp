b2598d46e09ab5160a3cd4fba139860b5e324167
YouruiGuo <yourui@ualberta.ca>
2019-04-13 22:34:39 -0600

    project CA* CD done

---
+++ project/astar.cpp
@@ -0,0 +1,434 @@
+//
+//  astar.cpp
+//  search-C
+//
+//  Created by Yourui Guo on 2019-03-08.
+//  Copyright © 2019 Yourui Guo. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+//#include <cstdlib>
+#include <cmath>
+#include <iterator>
+#include <queue>
+#include <map>
+#include <cmath>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <random>
+#include "grid_env.hpp"
+//#include "differentialHeuristic.hpp"
+
+typedef std::pair<float, int> weight_vertex_pair;
+
+class Heap{
+private:
+    std::deque<int> queue;
+    std::vector<StateInfo> *allStates;
+public:
+    Heap();
+    Heap(std::vector<StateInfo> *a);
+    ~Heap();
+    void heap_push(int index, float hcost);
+    int heap_pop();
+    void sift_up(int index);
+    void sift_down(int index);
+    int min_child(int index);
+    void heap_swap(int i, int j);
+    int getQueueLength();
+};
+
+
+Heap::Heap(std::vector<StateInfo> *a){
+    queue = {};
+    allStates = a;
+}
+Heap::Heap(){}
+Heap::~Heap(){}
+
+int Heap::getQueueLength() {
+    return (int)queue.size();
+}
+
+void Heap::heap_push(int index, float hcost) {
+    queue.push_back(index);
+    (*allStates)[index].open_id = (int)queue.size()-1;
+    (*allStates)[index].hcost = hcost;
+    sift_up((*allStates)[index].open_id);
+}
+
+int Heap::heap_pop() {
+    int result = queue.front();
+    queue.front() = queue.back();
+    queue.pop_back();
+    sift_down(0);
+    return result;
+}
+
+void Heap::sift_up(int index) {
+    
+    while (((index-1)/2 >= 0)) {
+        if (index == 0) {
+            break;
+        }
+        float a = (*allStates)[queue[index]].gcost+
+        (*allStates)[queue[index]].hcost;
+        float b = (*allStates)[queue[(index-1)/2]].gcost+
+        (*allStates)[queue[(index-1)/2]].hcost;
+        if (a == b) {
+            if ((*allStates)[queue[index]].gcost <
+                (*allStates)[queue[(index-1)/2]].gcost) {
+                heap_swap(index, (index-1)/2);
+            }
+        }
+        if (a < b) {
+            heap_swap(index, (index-1)/2);
+        }
+        index = (index-1)/2;
+    }
+}
+
+void Heap::sift_down(int index) {
+    while (index*2+1 < (int)queue.size()) {
+        int mc = min_child(index);
+        float a = (*allStates)[queue[index]].gcost+ // parent
+        (*allStates)[queue[index]].hcost;
+        float b = (*allStates)[queue[mc]].gcost+ // child
+        (*allStates)[queue[mc]].hcost;
+        if (a > b) {
+            heap_swap(index, mc);
+        }
+        index = mc;
+    }
+}
+
+int Heap::min_child(int index) {
+    if (index*2+2 >= (int)queue.size()) {
+        return index*2+1;
+    }
+    float a = (*allStates)[queue[index*2+1]].gcost+ // left child
+    (*allStates)[queue[index*2+1]].hcost;
+    float b = (*allStates)[queue[index*2+2]].gcost+ // fight child
+    (*allStates)[queue[index*2+2]].hcost;
+    if (a == b) {
+        if ((*allStates)[queue[index*2+1]].gcost >
+            (*allStates)[queue[index*2+2]].gcost) {
+            return index*2+1;
+        }
+        else return index*2+2;
+    }
+    if (a < b) {
+        return index*2+1;
+    }
+    return index*2+2;
+}
+
+void Heap::heap_swap(int i, int j) {
+    int temp = queue[i];
+    queue[i] = queue[j];
+    queue[j] = temp;
+    (*allStates)[queue[i]].open_id = i;
+    (*allStates)[queue[j]].open_id = j;
+}
+
+
+class Astar {
+private:
+    Heuristic heu = Heuristic();
+    //Voxel_env env = Voxel_env();
+    Grid_env env = Grid_env();
+    int start_index;
+    State start;
+    State goal;
+    Heap openlist;
+    std::vector<State> succes;
+    std::vector<int> parent, cur, act, isuccess_t, isuccess_g;
+public:
+    int expanded;
+    int updated;
+    int max_open;
+    std::vector<State> path;
+    Astar(State s, State g);
+    ~Astar();
+    bool canonicalAstar();
+    bool boundedJPS();
+    bool canonicalDijkstra();
+    bool canonicalWeightedAstar();
+    
+    std::vector<State> getPath();
+    Action prevAction(StateInfo st);
+};
+
+Astar::Astar(State s, State g) {
+    heu = Heuristic(s, g);
+    env = heu.getEnv();
+    start = s;
+    goal = g;
+    unsigned long long st = env.getStateHash(s);
+    start_index = env.getHashIndex(st);
+    env.allStates[start_index].gcost = 0;
+    env.allStates[start_index].hcost = heu.HCost(s);
+    std::vector<StateInfo> allStates = env.allStates;
+    openlist = Heap(&env.allStates);
+    max_open = 0;
+    expanded = 0;
+    updated = 0;
+}
+Astar::~Astar(){}
+
+Action Astar::prevAction(StateInfo st) {
+    bool isstart = true;
+    isuccess_t = st.getState().getState();
+    isuccess_g = start.getState();
+    for (unsigned int i=0; i<2; i++) {
+        if (isuccess_t[i] != isuccess_g[i]) {
+            isstart = false;
+        }
+    }
+    if (isstart) {
+        return Action({10, 10});
+    }
+    act.clear();
+    parent = env.allStates[st.parent].getState().getState();
+    cur = st.getState().getState();
+    for (int i = 0; i < parent.size(); i++) {
+        act.push_back(cur[i]-parent[i]);
+    }
+    return Action({act});
+}
+
+bool Astar::canonicalAstar() {
+    std::vector<Action> actions;
+    int next, open_id, index;
+    float newcost;
+    Action action = Action({0});
+    Action prevaction = Action({10, 10});
+    State start = env.getStart();
+    
+    openlist.heap_push(start_index, heu.HCost(start));
+    while (openlist.getQueueLength() != 0) {
+        index = openlist.heap_pop();
+        State st = env.allStates[index].getState();
+        env.allStates[index].open_id = -1;
+        expanded++;
+        if (env.isSuccess(st)) {
+            path = getPath();
+            return true;
+        }
+        prevaction = prevAction(env.allStates[index]);
+        env.getActions(st, prevaction, &actions);
+        for (std::vector<Action>::size_type i = 0; i < actions.size(); i++) {
+            action = actions[i];
+            next = env.applyActionCopy(action, st);
+            State next_state = env.allStates[next].getState();
+            open_id = env.allStates[next].open_id;
+            if (open_id == -1) {
+                continue;
+            }
+            else if (open_id > -1) {
+                newcost = env.allStates[index].gcost+
+                env.cost(st, next_state);
+                if (newcost < env.allStates[next].gcost) {
+                    updated++;
+                    env.allStates[next].gcost = newcost;
+                    env.allStates[next].parent = index;
+                    openlist.sift_up(env.allStates[next].open_id);
+                }
+            }
+            else {
+                env.allStates[next].gcost = env.allStates[index].gcost+
+                env.cost(st, next_state);
+                env.allStates[next].parent = index;
+                openlist.heap_push(next, heu.HCost(next_state));
+                max_open++;
+            }
+        }
+    }
+    return false;
+}
+
+
+bool Astar::boundedJPS() {
+    
+    return false;
+}
+
+bool Astar::canonicalDijkstra(){
+    int next, index, open_id;
+    StateInfo parentstate, currentstate;
+    float next_gcost, gcost;
+    std::vector<Action> actions;
+    bool isjp = false;
+    Action action = Action({0});
+    Action prevaction = Action({10, 10}), diprevaction = Action({10, 10});
+    
+    std::priority_queue<weight_vertex_pair,
+    std::vector<weight_vertex_pair>,
+    std::greater<weight_vertex_pair>> Q;
+    
+    expanded = 0;
+    State start = env.getStart();
+    openlist.heap_push(start_index, env.allStates[start_index].gcost);
+    while (openlist.getQueueLength() != 0) {
+        index = openlist.heap_pop();
+        Q.push(std::make_pair(0, index));
+        while (!Q.empty()) { // dijkstra's algorithm
+            gcost = Q.top().first;
+            index = Q.top().second;
+            Q.pop(); // pop the top element in queue
+            State st = env.allStates[index].getState();
+            env.allStates[index].open_id = -1; // add to closed list
+            expanded++;
+
+            parentstate = env.allStates[index];
+            prevaction = prevAction(parentstate);
+            // get all possible actions / successors
+            env.getActions(parentstate.getState(), prevaction, &actions);
+            
+            for (unsigned int i = 0; i < actions.size(); i++) {
+                action = actions[i];
+                next = env.applyActionCopy(action, st);
+                currentstate = env.allStates[next];
+                open_id = env.allStates[next].open_id;
+                next_gcost = env.cost(currentstate.getState(), parentstate.getState());
+
+                isjp = env.isJumpPoint(currentstate.getState(), action);
+                
+                if (open_id == -1) { // closed list
+                    if (currentstate.gcost > parentstate.gcost + next_gcost) {
+                        // update the g-cost
+                        env.allStates[next].gcost = next_gcost + env.allStates[index].gcost;
+                        env.allStates[next].parent = index;
+                        updated++;
+                    }
+                    continue;
+                }
+                
+                if (isjp) {
+                    env.allStates[next].gcost = next_gcost + env.allStates[index].gcost;
+                    env.allStates[next].parent = index;
+                    if (env.allStates[next].open_id == -10 || env.allStates[next].open_id == -1) {
+                        openlist.heap_push(next, env.allStates[next].gcost);
+                    }
+                    else {
+                        openlist.sift_up(env.allStates[next].open_id);
+                    }
+                }
+                else {
+                    // generate new state
+                    env.allStates[next].gcost = next_gcost + env.allStates[index].gcost;
+                    env.allStates[next].parent = index;
+                    Q.push(std::make_pair(env.allStates[next].gcost, next));
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool Astar::canonicalWeightedAstar(){
+    
+    
+    return false;
+}
+
+std::vector<State> Astar::getPath() {
+    float pathcost = 0;
+    path.clear();
+    path.push_back(env.getGoal());
+    unsigned long long v = env.getStateHash(env.getGoal());
+    StateInfo state = env.allStates[env.getHashIndex(v)];
+    do {
+        //cout << state.getState().getState()[0] << ' ' << state.getState().getState()[1] << ' ' << state.getState().getState()[2] << endl;
+        pathcost += env.cost(state.getState(), env.allStates[state.parent].getState());
+        path.push_back(env.allStates[state.parent].getState());
+        state = env.allStates[state.parent];
+    }while (state.parent != -1);
+    //cout << pathcost << endl;
+    return path;
+}
+
+void load3dfile(int index, std::vector<int> *s, std::vector<int> *g){
+    s->clear();
+    g->clear();
+    std::vector<State> instances;
+    std::string df = "/Users/margaret/Documents/cmput652/searchAlg/data/Simple.3dmap.3dscen";
+    std::ifstream infile(df);
+    std::string line;
+    int count = -1;
+    while (std::getline(infile, line)) {
+        if (count++ == index) {
+            std::istringstream iss(line);
+            std::vector<std::string> tokens{std::istream_iterator<std::string>(iss),
+                std::istream_iterator<std::string>()};
+            s->push_back(std::stoi(tokens[0]));
+            s->push_back(std::stoi(tokens[1]));
+            s->push_back(std::stoi(tokens[2]));
+            g->push_back(std::stoi(tokens[3]));
+            g->push_back(std::stoi(tokens[4]));
+            g->push_back(std::stoi(tokens[5]));
+            break;
+        }
+    }
+}
+
+void loadGridFile(int index, std::vector<int> *s, std::vector<int> *g) {
+    s->clear();
+    g->clear();
+    std::vector<State> instances;
+    std::string df = "/Users/margaret/Documents/cmput652/search-C1/asg4/data/brc101d.map.scen";
+    std::ifstream infile(df);
+    std::string line;
+    int count = -1;
+    while (std::getline(infile, line)) {
+        if (count++ == index) {
+            std::istringstream iss(line);
+            std::vector<std::string> tokens{std::istream_iterator<std::string>(iss),
+                std::istream_iterator<std::string>()};
+            s->push_back(std::stoi(tokens[4]));
+            s->push_back(std::stoi(tokens[5]));
+            g->push_back(std::stoi(tokens[6]));
+            g->push_back(std::stoi(tokens[7]));
+            break;
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    //std::vector<int> g = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    //State goal = State(g);
+    std::vector<int> v;
+    
+    
+    for (int i = 0; i < 1570; ++i){
+        std::vector<int> s, g;
+        //load3dfile(i, &s, &g);
+        loadGridFile(i, &s, &g);
+        Astar search = Astar(s, g);
+        clock_t begin = clock();
+        bool result = search.canonicalAstar();
+        //bool result = search.canonicalDijkstra();
+        clock_t end = clock();
+        //std::queue<State> *path = search.getPath();
+        if (result == true){
+            std::cout << "num: " << i <<" expanded: " << search.expanded << " updated: " <<
+            search.updated << " path_len: " << search.path.size() << " max_open: " << search.max_open << " time_elpased: "
+            << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+        }
+        //break;
+    }
+    
+    //State benchmark = loadstpFile(index, &si);
+    //std::vector<int> s = {1,2,3,7,0,4,5,6,8,9,10,11,12,13,14,15};
+    //State benchmark = State(s);
+    
+    return 0;
+}
---
+++ project/grid_env.hpp
@@ -0,0 +1,465 @@
+//
+//  grid_env.hpp
+//  astar
+//
+//  Created by Yourui Guo on 2019-03-25.
+//  Copyright © 2019 Yourui Guo. All rights reserved.
+//
+
+#ifndef grid_env_h
+#define grid_env_h
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <map>
+#include <cmath>
+#include "state.hpp"
+
+
+class Action
+{
+private:
+    std::vector<int> action;
+public:
+    Action(std::vector<int> a);
+    ~Action();
+    std::vector<int> getAction();
+    void setAction(std::vector<int> a);
+};
+
+Action::Action(std::vector<int> a) {
+    action = a;
+}
+
+Action::~Action(){}
+
+std::vector<int> Action::getAction() {
+    return action;
+}
+
+void Action::setAction(std::vector<int> a) {
+    action = a;
+}
+
+class Grid_env {
+    State start_state;
+    State goal_state;
+    std::vector<int> mapsize; // mapsize[0]:height mapsize[1]: width
+    std::vector<Action> singleAction, allActions, jpactions;
+    
+    float gcost;
+    std::vector<int> isuccess_t, isuccess_g, inmap, hashtemp, costp, costc;
+    std::vector<int> isrepeat_p, isrepeat_c, lx, ly, act, acttemp;
+    std::vector<std::vector<int>> jp;
+public:
+    double inf;
+    std::map<unsigned long long, int> hashtable;
+    std::vector<StateInfo> allStates;
+    Grid_env(State s, State g);
+    Grid_env();
+    ~Grid_env();
+    void setStart(State s);
+    void setGoal(State g);
+    State getStart();
+    State getGoal();
+    bool isSuccess(State st);
+    bool isRepeat(Action prev, Action curr);
+    bool isInMap(State st);
+    unsigned long long getStateHash(State st);
+    void getAllActions(State st, Action prev);
+    bool isJumpPoint(State st, Action prev);
+    void getActions(State st, Action prev, std::vector<Action> *actions);
+    int applyActionCopy(Action a, State s); //return index of stateinfo list
+    void applyAction(Action a, State *s); //return cost
+    void undoAction(Action a, State *s);
+    double cost(State prev, State curr);
+    void loadMap();
+    std::vector<int> getMapsize();
+    int getHashIndex(unsigned long long i);
+    State unranking(unsigned long long h);
+};
+
+Grid_env::Grid_env(){}
+Grid_env::~Grid_env(){}
+
+Grid_env::Grid_env(State s, State g) {
+    start_state = s;
+    goal_state = g;
+    //mapsize = si;
+    inf = std::numeric_limits<double>::infinity();
+    mapsize.resize(2, 0);
+    loadMap();
+    
+    allStates.push_back(s);
+    unsigned long long svalue = getStateHash(s);
+    hashtable[svalue] = (int)allStates.size()-1;
+    
+    allStates.push_back(g);
+    unsigned long long gvalue = getStateHash(g);
+    hashtable[gvalue] = (int)allStates.size()-1;
+    
+}
+
+void Grid_env::setStart(State s) {
+    start_state = s;
+}
+
+void Grid_env::setGoal(State g) {
+    goal_state = g;
+}
+
+State Grid_env::getStart() {
+    return start_state;
+}
+
+State Grid_env::getGoal() {
+    return goal_state;
+}
+
+bool Grid_env::isSuccess(State st) {
+    isuccess_t = st.getState();
+    isuccess_g = goal_state.getState();
+    for (unsigned int i=0; i<2; i++) {
+        if (isuccess_t[i] != isuccess_g[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Grid_env::isRepeat(Action prev, Action curr) {
+    int count = 0;
+    isrepeat_p = prev.getAction();
+    isrepeat_c = curr.getAction();
+    if (isrepeat_p[0] == -isrepeat_c[0]) {
+        count++;
+    }
+    if (isrepeat_p[1] == -isrepeat_c[1]) {
+        count++;
+    }
+    if (count == 2) {
+        return true;
+    }
+    return false;
+}
+
+bool Grid_env::isInMap(State st) {
+    inmap = st.getState();
+    if ((inmap[0] < 0) || (inmap[0] >= mapsize[0])) {
+        return false;
+    }
+    if ((inmap[1] < 0) || (inmap[1] >= mapsize[1])) {
+        return false;
+    }
+    return true;
+}
+
+unsigned long long Grid_env::getStateHash(State st) {
+    hashtemp = st.getState();
+    unsigned long long value = 0;
+    value = hashtemp[0]*mapsize[1]+hashtemp[1];
+    return value;
+}
+
+State Grid_env::unranking(unsigned long long h) {
+    hashtemp[1] = h % mapsize[1];
+    hashtemp[0] = int(h / mapsize[1]);
+    return State(hashtemp);
+}
+
+void Grid_env::getAllActions(State st, Action prev) {
+
+    bool isjp = isJumpPoint(st, prev);
+    act = prev.getAction();
+    
+    lx.clear();
+    lx.push_back(0);
+    ly.clear();
+    ly.push_back(0);
+    
+    if (isjp) {
+        for (int i = 0; i < jp.size(); i++) {
+            if (jp[i][0] != 0) {
+                lx.push_back(jp[i][0]);
+            }
+            else if (jp[i][1] != 0) {
+                ly.push_back(jp[i][1]);
+            }
+        }
+    }
+    
+    if (act[0] != 0) {
+        if (act[0] == 10) { // if is the start state
+            lx.push_back(-1);
+            lx.push_back(1);
+        }
+        else{
+            lx.push_back(act[0]);
+        }
+    }
+    if (act[1] != 0) {
+        if (act[1] == 10) { // if is the start state
+            ly.push_back(-1);
+            ly.push_back(1);
+        }
+        else {
+            ly.push_back(act[1]);
+        }
+    }
+    allActions.clear();
+    for (const int x : lx) {
+        for (const int y : ly) {
+            if (x==0 && y==0) {
+                continue;
+            }
+            allActions.push_back(Action({x, y}));
+        }
+    }
+}
+
+bool Grid_env::isJumpPoint(State st, Action prev) {
+    bool isjp = false;
+    jp.clear();
+    bool cur[2] = {false}, pre[2] = {false};
+    unsigned long long hv;
+    acttemp.clear();
+    acttemp = prev.getAction();
+
+    if (acttemp[0] == 0 || acttemp[1] == 0) {
+        jpactions.clear();
+        if (acttemp[0] == 0) {
+            jpactions.push_back(Action({1,0}));
+            jpactions.push_back(Action({-1,0}));
+        }
+        else {
+            jpactions.push_back(Action({0,1}));
+            jpactions.push_back(Action({0,-1}));
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            applyAction(jpactions[i], &st);
+            hv = getStateHash(st);
+            std::map<unsigned long long, int>::iterator f =
+            hashtable.find(hv);
+            if (f != hashtable.end()) {
+                if ((allStates[hashtable[hv]].gcost == -1) || !isInMap(st)){
+                    cur[i] = true;
+                }
+            }
+            undoAction(jpactions[i], &st);
+        }
+        
+        undoAction(prev, &st);
+        
+        for (int i = 0; i < 2; i++) {
+            applyAction(jpactions[i], &st);
+            hv = getStateHash(st);
+            std::map<unsigned long long, int>::iterator f =
+            hashtable.find(hv);
+            if (f != hashtable.end()) {
+                if ((allStates[hashtable[hv]].gcost == -1) || !isInMap(st)){
+                    pre[i] = true;
+                }
+            }
+            undoAction(jpactions[i], &st);
+        }
+        applyAction(prev, &st);
+    }
+    if (pre[0] && !cur[0]) {
+        isjp = true;
+        jp.push_back(jpactions[0].getAction());
+    }
+    if (pre[1] && !cur[1]) {
+        isjp = true;
+        jp.push_back(jpactions[1].getAction());
+    }
+    if (isjp) {
+        return isjp;
+    }
+    return isjp;
+}
+
+void Grid_env::getActions(State st, Action prev, std::vector<Action> *actions) {
+    actions->clear();
+    bool filled;
+    unsigned long long hv;
+    getAllActions(st, prev);
+    
+    for (int i = 0; i < (int)allActions.size(); i++) {
+        filled = false;
+        singleAction.clear();
+        lx.clear();
+        lx.push_back(0);
+        ly.clear();
+        ly.push_back(0);
+        act = allActions[i].getAction();
+        if (act[0] != 0) {
+            lx.push_back(act[0]);
+        }
+        if (act[1] != 0) {
+            ly.push_back(act[1]);
+        }
+        for (const int x : lx) {
+            for (const int y : ly) {
+                if (x==0 && y==0) {
+                    continue;
+                }
+                applyAction(Action({x,y}), &st);
+                hv = getStateHash(st);
+                gcost = 0;
+                std::map<unsigned long long, int>::iterator f =
+                hashtable.find(hv);
+                if (f != hashtable.end()) {
+                    gcost = allStates[hashtable[hv]].gcost;
+                }
+                if (gcost == -1) {
+                    filled = true;
+                }
+                if (!isInMap(st)) {
+                    filled = true;
+                }
+                undoAction(Action({x,y}), &st);
+            }
+        }
+        if (!filled) {
+            actions->push_back(allActions[i]);
+        }
+    }
+}
+
+int Grid_env::applyActionCopy(Action a, State s) {
+    std::vector<int> tempst = s.getState();
+    tempst[0] += a.getAction()[0];
+    tempst[1] += a.getAction()[1];
+    State newst = State(tempst);
+    unsigned long long hv = getStateHash(newst);
+    std::map<unsigned long long, int>::iterator f =
+    hashtable.find(hv);
+    if (f != hashtable.end()) {
+        return hashtable[hv];
+    }
+    allStates.push_back(StateInfo(newst));
+    hashtable[hv] = (int)allStates.size()-1;
+    return hashtable[hv];
+}
+
+void Grid_env::applyAction(Action a, State *s) {
+    std::vector<int> tempst = s->getState();
+    tempst[0] += a.getAction()[0];
+    tempst[1] += a.getAction()[1];
+    s->setState(tempst);
+}
+
+void Grid_env::undoAction(Action a, State *s) {
+    std::vector<int> tempst = s->getState();
+    tempst[0] -= a.getAction()[0];
+    tempst[1] -= a.getAction()[1];
+    s->setState(tempst);
+}
+
+void Grid_env::loadMap() {
+    std::vector<int> s;
+    s.resize(2, 0);
+    State st;
+    unsigned long long h;
+    std::string filename = "/Users/margaret/Documents/cmput652/search-C1/asg4/data/brc101d.map";
+    std::ifstream src (filename);
+
+    int line = 0;
+    std::string input;
+    while (line < 4 && std::getline(src, input)) {
+        if (line == 1 || line == 2){
+            std::stringstream iss(input);
+            std::vector<std::string> tokens{std::istream_iterator<std::string>(iss),
+                std::istream_iterator<std::string>()};
+            mapsize[2-line] = std::stoi(tokens[1]);
+        }
+        line++;
+    }
+    line = 0;
+    while (src && line < mapsize[1]) {
+        std::getline (src, input);
+        for (size_t i = 0; i < mapsize[0]; i++) {
+            if (input[i] != '.') {
+                s.clear();
+                s.push_back((int)i);
+                s.push_back(line);
+                st = State(s);
+                h = getStateHash(st);
+                allStates.push_back(StateInfo(st));
+                hashtable[h] = (int)allStates.size() - 1;
+                allStates[hashtable[h]].gcost = -1;
+            }
+        }
+        line++;
+    }
+}
+
+double Grid_env::cost(State prev, State curr) {
+    double value = 0;
+    costc = curr.getState();
+    costp = prev.getState();
+    value += abs(costc[0] - costp[0]);
+    value += abs(costc[1] - costp[1]);
+    return sqrt(value);
+}
+
+int Grid_env::getHashIndex(unsigned long long i) {
+    return hashtable[i];
+}
+
+
+std::vector<int> Grid_env::getMapsize() {
+    return mapsize;
+}
+
+class Heuristic {
+private:
+    //Voxel_env env = Voxel_env();
+    Grid_env env = Grid_env();
+    unsigned long long numExpandNode, totalnum;
+    std::vector<int> mapSize, tempstate1, tempstate2;
+public:
+    Heuristic();
+    Heuristic(State s, State g);
+    ~Heuristic();
+    //Voxel_env getEnv();
+    Grid_env getEnv();
+    float HCost(State st);
+};
+Heuristic::Heuristic(State s, State g) {
+    //env = Voxel_env(s, g);
+    env = Grid_env(s, g);
+    numExpandNode = 0;
+    totalnum = 1;
+    mapSize = env.getMapsize();
+    
+}
+Heuristic::Heuristic(){}
+Heuristic::~Heuristic(){}
+/*
+ Voxel_env Heuristic::getEnv() {
+ return env;
+ }
+ */
+Grid_env Heuristic::getEnv() {
+    return env;
+}
+
+float Heuristic::HCost(State st) {
+    float x,y,dmin,dmax;
+    tempstate1 = st.getState();
+    tempstate2 = env.getGoal().getState();
+    x = abs(tempstate1[0] - tempstate2[0]);
+    y = abs(tempstate1[1] - tempstate2[1]);
+    dmin = fmin(x, y);
+    dmax = fmax(x, y);
+    return (sqrt(2)-1)*dmin + dmax;
+}
+
+#endif /* grid_env_h */
